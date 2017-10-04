05821cb158ffa17c919b294a8ea7f0e3561007ab
E66666666 <emily66666666ok@outlook.com>
2017-10-04 16:13:26 +0000

    Fix ln

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 # Put the files in the right places
 # This script expects to be in lolisafe/pages/custom/
 
-ln -s css_custom ../../public/css_custom
-ln -s images_custom ../../public/images_custom
+ln -s "$(dirname "$(realpath "$0")")"/css_custom ../../public/css_custom
+ln -s "$(dirname "$(realpath "$0")")"/images_custom ../../public/images_custom
 
 cp -TRf error ../error
 
